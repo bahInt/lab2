0314ecb8f2a09a7ac3a764dacb85eeb646641248
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 15:48:52 +0300

    Scripted auto-commit on change (2021-12-14 15:48:52) by gitwatch.sh

--- src/main/java/lab2/JobApp.java
+++ src/main/java/lab2/JobApp.java
@@ -4,6 +4,7 @@ public class JobApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: App <input path> <output path>");
+            
         }
     }
 }
