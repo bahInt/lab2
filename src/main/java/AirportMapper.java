4d17db32d5c6a2543d4a3d7795015b5607e6fcb5
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 11:40:32 +0300

    Scripted auto-commit on change (2021-12-14 11:40:32) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -2,7 +2,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
-    protected void map(){}
+    protected void map() throws IOException {
+        
+    }
 }
