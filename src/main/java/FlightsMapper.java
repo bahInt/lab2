b005f4fe71f0b0c65ff565d9c25ba4eafeff1143
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 12:44:01 +0300

    Scripted auto-commit on change (2021-12-14 12:44:01) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -2,7 +2,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightsMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
-    protected void map(LongWritable)
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] column = line.split(",");
+        String airport
+    }
 }
