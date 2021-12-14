a6acee2c0607fa0da6cfb8f04ff9181166909d2c
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 14:42:25 +0300

    Scripted auto-commit on change (2021-12-14 14:42:25) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -9,6 +9,12 @@ import java.util.Iterator;
 public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> value, Context context) throws IOException, InterruptedException {
-        Iterator<Text> iter = values
+        Iterator<Text> iter = value.iterator();
+        Text systeminfo = new Text(iter.next);
+        while(iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systeminfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
     }
 }
