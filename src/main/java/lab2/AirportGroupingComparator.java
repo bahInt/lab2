c70db52b0eae775c3d8a80eb2c1e336f07709859
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 14:12:03 +0300

    Scripted auto-commit on change (2021-12-14 14:12:03) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -3,13 +3,13 @@ package lab2;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public static class AirportGroupingComparator extends WritableComparator {
+public class AirportGroupingComparator extends WritableComparator {
     protected  AirportGroupingComparator() {
         super(AirportWritableComparable.class, true);
     }
 
     @Override
-    public int compare(AirportWritableComparable first, AirportWritableComparable b1) {
+    public int compare(AirportWritableComparable first, AirportWritableComparable second) {
         AirportWritableComparable fistAirportID = (AirportWritableComparable) first;
         AirportWritableComparable secondAirportID = (AirportWritableComparable) second;
 
