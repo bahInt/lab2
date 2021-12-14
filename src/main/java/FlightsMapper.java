0395945a1cbed185f6b2683a3e15319a3fe0d806
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 12:52:10 +0300

    Scripted auto-commit on change (2021-12-14 12:52:10) by gitwatch.sh

--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -5,10 +5,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightsMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    private static final int DEST_AIRPORT_ID = 14;
+    private static final int TABLE_DESCRIPTION = 0;
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] column = line.split(",");
-        String airport
+        String destAirportIDString = column[DEST_AIRPORT_ID];
+
+        if(key.get() != TABLE_DESCRIPTION){
+            int destAirportID = Integer.parseInt(destAirportIDString);
+            String destDelay = column[];
+        }
     }
 }
