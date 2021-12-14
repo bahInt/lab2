398fad93846bbd6cc735d3f294ac42432633945b
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 14:36:24 +0300

    Scripted auto-commit on change (2021-12-14 14:36:24) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -1,4 +1,5 @@
 package lab2;
 
-public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, Text> { @Override
+public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
+    @Override
 }
