0d1c4c808ec0b3cb16cfa523ca95143617f5b628
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 11:30:11 +0300

    Scripted auto-commit on change (2021-12-14 11:30:11) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -1,4 +1,6 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper extends Mapper<> {
+public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 }
--- src/main/java/AirportWritableComparable.java
+++ src/main/java/AirportWritableComparable.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class AirportWritableComparable {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritableComparable implements WritableComparable<AirportWritableComparable> {
+    private IntWritable airportId;
+    private IntWritable dataIndex;
+
+    protected IntWritable getAirportId(){
+        return this.airportId;
+    }
+    protected IntWritable getDataIndex(){
+        return this.dataIndex;
+    }
+
+    public void AirportWritableComparable(IntWritable airportId, IntWritable dataIndex){
+        this.airportId = airportId;
+        this.dataIndex = dataIndex;
+
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        airportId.write(dataOutput);
+        airportId.write(dataOutput);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        airportId.readFields(dataInput);
+        dataIndex.readFields(dataInput);
+    }
+
+    @Override
+    public int compareTo(AirportWritableComparable o) {
+        int compareAirportId = this.getAirportId().compareTo(
+                o.getAirportId()
+        );
+        if (compareAirportId == 0){
+            return this.getDataIndex().compareTo(
+                    o.getDataIndex()
+            );
+        }
+        return compareAirportId;
+    }
 }
