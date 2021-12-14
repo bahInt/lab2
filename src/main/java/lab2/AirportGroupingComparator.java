c2dd50f3e507c837735b319f1ea6d6ac18e0fb6c
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 14:03:59 +0300

    Scripted auto-commit on change (2021-12-14 14:03:59) by gitwatch.sh

--- src/main/java/lab2/AirportGroupingComparator.java
+++ src/main/java/lab2/AirportGroupingComparator.java
@@ -11,6 +11,6 @@ public static class AirportGroupingComparator extends WritableComparator {
     @Override
     public int compare(AirportWritableComparable a1, AirportWritableComparable b1) {
         AirportWritableComparable a = (AirportWritableComparable) a1;
-        AirportWritableComparable b = (AirportWritableComparable) b1;
+        AirportWritableComparable
     }
 }
