2c31a8a9311ccccb1765a7b490a245b8063308a8
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 14:38:12 +0300

    Scripted auto-commit on change (2021-12-14 14:38:12) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -3,6 +3,11 @@ package lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
     @Override
+    protected void reduce() throws IOException, InterruptedException {
+        
+    }
 }
