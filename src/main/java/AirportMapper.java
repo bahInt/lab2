eef675baffd5cdfa26afaa4dfab673230d425ef8
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 11:54:17 +0300

    Scripted auto-commit on change (2021-12-14 11:54:17) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] line = value.toString();
+        String[] line = value.toString().split(",[\"]");
+        if(key.get() != 0){
+            String 
+        }
     }
 }
