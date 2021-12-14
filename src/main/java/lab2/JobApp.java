3dfb82661f6aced5168d41b37ac09fbe1a55047f
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 15:56:14 +0300

    Scripted auto-commit on change (2021-12-14 15:56:14) by gitwatch.sh

--- src/main/java/lab2/JobApp.java
+++ src/main/java/lab2/JobApp.java
@@ -3,6 +3,7 @@ package lab2;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 public class JobApp {
     public static void main(String[] args) throws Exception {
@@ -13,6 +14,6 @@ public class JobApp {
         Job job = Job.getInstance();
         job.setJarByClass(JobApp.class);
         job.setJobName("Join");
-        MultipleInputs.addInputPath(job, new Path(args[0]));
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
     }
 }
