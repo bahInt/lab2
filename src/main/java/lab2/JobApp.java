de22aad77ad190967535ba26d821aaf27397ce37
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-14 15:49:36 +0300

    Scripted auto-commit on change (2021-12-14 15:49:36) by gitwatch.sh

--- src/main/java/lab2/JobApp.java
+++ src/main/java/lab2/JobApp.java
@@ -1,10 +1,13 @@
 package lab2;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class JobApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: App <input path> <output path>");
-            
+            System.exit(-1);
         }
+        Job job = Job.getInstance();
     }
 }
