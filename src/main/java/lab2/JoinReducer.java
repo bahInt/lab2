8a8b6f1204da15a0532d2d4dddb439c76c5e70b7
Bakhtin Igor <skilful2000@yandex.ru>
2021-12-15 10:24:53 +0300

    Scripted auto-commit on change (2021-12-15 10:24:53) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -26,6 +26,7 @@ public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, 
                     if(maxDelay < iterDelay) {
                         maxDelay = iterDelay;
                     }
+                    
                     sumDelays += iterDelay;
                     amountOfValues++;
                 }
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ hadoop fs -rm -r output
 hadoop fs -put ~/IdeaProjects/lab2/L_AIRPORT_ID.csv
 hadoop fs -put ~/IdeaProjects/lab2/664600583_T_ONTIME_sample.csv
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
-hadoop lab2.JobApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output
-hadoop jar ./target/hadoop-examples-1.0-SNAPSHOT.jar lab2.JobApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv  output
-hadoop fs -copyToLocal output
+hadoop lab2.JobApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv outputLab2
+hadoop jar ./target/hadoop-examples-1.0-SNAPSHOT.jar lab2.JobApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv  outputLab2
+hadoop fs -copyToLocal outputLab2
 exit 0
